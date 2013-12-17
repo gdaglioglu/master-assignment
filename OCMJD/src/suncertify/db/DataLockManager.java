57bfcd2d244da1fdb6f5d928bc1307ba4171c1c3
Eoin Mooney <eoinm@esatclear.ie>
2013-12-17 18:41:11 +0000

    Documented DataLockManager.java

--- OCMJD/src/suncertify/db/DataLockManager.java
+++ OCMJD/src/suncertify/db/DataLockManager.java
@@ -1,30 +1,33 @@
 /*
+ * DataLockManager
  * 
+ * Software developed for Oracle Certified Master, Java SE 6 Developer
  */
 package suncertify.db;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Logger;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class DataLockManager.
+ * This singleton class is a worker class called by the <code>Data</code> facade class to provide a locking mechanism for the application
+ * 
+ * @author Eoin Mooney
  */
 public class DataLockManager {
 
-	/** The log. */
-	private final Logger log = Logger.getLogger("suncertify.db");
-
-	/** The locked records. */
+	/**
+	 * Hash Map containing records and their locks
+	 */
 	private final Map<Long, Long> lockedRecords = new HashMap<Long, Long>();
 
-	/** The Constant instance. */
+	/**
+	 * Creates a singleton instance of the class
+	 */
 	private static final DataLockManager instance = new DataLockManager();
 
 	/**
 	 * Gets the single instance of DataLockManager.
-	 *
+	 * 
 	 * @return single instance of DataLockManager
 	 */
 	public static DataLockManager getInstance() {
@@ -32,22 +35,25 @@ public class DataLockManager {
 	}
 
 	/**
-	 * Instantiates a new data lock manager.
+	 * Private constructor for this singleton class
 	 */
 	private DataLockManager() {
 	}
 
 	/**
-	 * Lock record.
-	 *
-	 * @param recNo the rec no
-	 * @return the long
-	 * @throws RecordNotFoundException the record not found exception
+	 * Locks a record so that it can only be updated by this user. If record is
+	 * already locked, user's thread will give up CPU until the record is
+	 * becomes unlocked
+	 * 
+	 * @param recNo
+	 *            The record number of the record to be locked
+	 * @return A <code>lockCookie</code> to ensure only one user can update a
+	 *         record at a time
+	 * @throws Signals
+	 *             that no valid record exists with given record number
 	 */
 	public synchronized long lockRecord(final long recNo)
 			throws RecordNotFoundException {
-		this.log.entering("suncertify.db.DataLockManager", "lockRecord()");
-
 		final long lockOwnerCookie = Thread.currentThread().getId();
 		try {
 
@@ -64,22 +70,23 @@ public class DataLockManager {
 		}
 		this.lockedRecords.put(recNo, lockOwnerCookie);
 
-		this.log.exiting("suncertify.db.DataLockManager", "lockRecord()");
-
 		return lockOwnerCookie;
 	}
 
 	/**
-	 * Unlock.
-	 *
-	 * @param recNo the rec no
-	 * @param cookie the cookie
-	 * @throws SecurityException the security exception
+	 * Unlocks a previously locked record.
+	 * 
+	 * @param recNo
+	 *            The record number of the record to be unlocked
+	 * @param cookie
+	 *            A <code>lockCookie</code> previously provided by this class to
+	 *            ensure only one user can update a record at a time
+	 * @throws SecurityException
+	 *             Signals that user is attempting to unlock a record with an
+	 *             invalid <code>lockCookie</code>
 	 */
 	public synchronized void unlock(final long recNo, final long cookie)
 			throws SecurityException {
-		this.log.entering("suncertify.db.DataLockManager", "unlock()");
-
 		final long lockOwnerCookie = Thread.currentThread().getId();
 
 		if (!this.isLocked(recNo)) {
@@ -96,24 +103,26 @@ public class DataLockManager {
 			throw new SecurityException(
 					"Provided lock cookie does not match user generated lock cookie");
 		}
-		this.log.exiting("suncertify.db.DataLockManager", "unlock()");
 	}
 
 	/**
-	 * Checks if is locked.
-	 *
-	 * @param recNo the rec no
-	 * @return true, if is locked
+	 * Checks if a lock exists for a given record.
+	 * 
+	 * @param recNo
+	 *            The record number of the record to be checked
+	 * @return true, if record is locked
 	 */
 	public boolean isLocked(final long recNo) {
 		return this.lockedRecords.containsKey(recNo);
 	}
 
 	/**
-	 * Gets the owner.
-	 *
-	 * @param recNo the rec no
-	 * @return the owner
+	 * Gets the lockCookie associated with a record
+	 * 
+	 * @param recNo
+	 *            The record number of the record to be checked
+	 * @return The <code>lockCookie</code> of the record if it is locked,
+	 *         otherwise <code>null</code>
 	 */
 	public Long getOwner(final long recNo) {
 		return this.lockedRecords.get(recNo);
