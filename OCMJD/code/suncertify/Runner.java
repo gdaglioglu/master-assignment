63ca1b8a0ca7e9261a1d05806d669250e0df61b4
Eoin Mooney <eoinm@esatclear.ie>
2013-12-17 20:20:09 +0000

    Documented Runner.java and organised code for submission

--- OCMJD/code/suncertify/Runner.java
+++ OCMJD/code/suncertify/Runner.java
@@ -3,43 +3,36 @@
  * 
  * Software developed for Oracle Certified Master, Java SE 6 Developer
  */
-package suncertify.main;
-
-import java.util.logging.*;
+package suncertify;
 
 import suncertify.ui.ClientController;
 import suncertify.ui.Server;
 import suncertify.util.ApplicationMode;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class Runner.
+ * This class is the entry point for the application.
+ * 
+ * @author Eoin Mooney
  */
 public class Runner {
 
-	/** The log. */
-	private static Logger log = Logger.getLogger("suncertify");
-
-	/** The handler. */
-	private static ConsoleHandler handler = new ConsoleHandler();
-
 	/**
 	 * The main method.
-	 *
-	 * @param args the arguments
+	 * 
+	 * @param args
+	 *            the arguments
 	 */
 	public static void main(final String[] args) {
-		Runner.log.setLevel(Level.ALL);
-		Runner.handler.setLevel(Level.ALL);
-		Runner.log.addHandler(Runner.handler);
-
 		new Runner(args);
 	}
 
 	/**
-	 * Instantiates a new runner.
-	 *
-	 * @param args the args
+	 * Instantiates a new runner and starts the application based on the
+	 * arguments it receives
+	 * 
+	 * @param args
+	 *            This application will only accept "", "alone" or "server" as
+	 *            valid arguments
 	 */
 	public Runner(final String[] args) {
 		if (args.length == 0) {
