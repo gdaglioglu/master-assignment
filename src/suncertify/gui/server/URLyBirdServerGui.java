519fdd062c3e6a5071ba1094abd393d346b9c48d
Luke GJ Potter <lukegjpotter@gmail.com>
2014-05-03 01:55:37 +0100

    Server GUI validating inputs. Updating inputs that differ from the properties file.
    Removing header from suncertify.properties file, as it would get overwritten by a change in the properties.

--- src/suncertify/gui/server/URLyBirdServerGui.java
+++ src/suncertify/gui/server/URLyBirdServerGui.java
@@ -1,15 +1,25 @@
 package suncertify.gui.server;
 
+import suncertify.utilities.URLyBirdApplicationConstants;
 import suncertify.utilities.URLyBirdApplicationGuiConstants;
+import suncertify.utilities.URLyBirdApplicationObjectsFactory;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * @author Luke GJ Potter
  *         Date: 29/04/2014
  */
 public class UrlyBirdServerGui extends JFrame {
+
+    private ServerConfigurationPanel serverConfigurationPanel;
 
     public UrlyBirdServerGui() {
 
@@ -20,7 +30,81 @@ public class UrlyBirdServerGui extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        add(new ServerConfigurationPanel().getServerConfigurationPanel(), BorderLayout.CENTER);
-        add(new ServerControlPanel().getServerControlPanel(), BorderLayout.SOUTH);
+        serverConfigurationPanel = new ServerConfigurationPanel();
+
+        add(serverConfigurationPanel.getServerConfigurationPanel(), BorderLayout.CENTER);
+        add(getServerControlPanel(), BorderLayout.SOUTH);
+    }
+
+    private JPanel getServerControlPanel() {
+
+        JPanel serverControlPanel = new JPanel(new FlowLayout());
+        JButton startServerButton = new JButton(URLyBirdApplicationGuiConstants.START_SERVER_BUTTON);
+        startServerButton.addActionListener(new StartServer());
+        serverControlPanel.add(startServerButton);
+
+        return serverControlPanel;
+    }
+
+    private class StartServer implements ActionListener {
+
+        @Override public void actionPerformed(ActionEvent actionEvent) {
+
+            if (!serverConfigurationPanel.areTextFieldValuesValid()) {
+                showErrorMessageDialog("Please enter valid values in the Text Fields.");
+                return;
+            }
+
+            try {
+                updatePropertiesToReflectServerGui();
+            } catch (FileNotFoundException ignored) {
+                showErrorMessageDialog("The Application's Properties File does not exist.");
+                return;
+            } catch (IOException ignored) {
+                showErrorMessageDialog("There was an error updating the Application's Properties File.");
+                return;
+            }
+
+            // TODO: Implement RMI
+            JOptionPane.showMessageDialog(null, "Such Correct, Much Valid, Wow.", "Success", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        private void showErrorMessageDialog(String message) {
+            JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        private void updatePropertiesToReflectServerGui() throws IOException {
+
+            Properties properties = URLyBirdApplicationObjectsFactory.getURLyBirdApplicationProperties();
+
+            if (hasDatabasePathPropertyChanged(properties)) {
+
+                properties.setProperty(
+                        URLyBirdApplicationConstants.PROPERTY_FILE_KEY_PATH_TO_DATABASE_FILE,
+                        serverConfigurationPanel.getPathToDatabaseFileFromTextField());
+
+                properties.setProperty(
+                        URLyBirdApplicationConstants.PROPERTY_FILE_KEY_RMI_HOSTNAME,
+                        serverConfigurationPanel.getRmiHostnameFromTextField());
+
+                properties.setProperty(
+                        URLyBirdApplicationConstants.PROPERTY_FILE_KEY_RMI_PORT_NUMBER,
+                        serverConfigurationPanel.getRmiPortNumberFromTextField());
+
+                properties.store(new FileOutputStream(URLyBirdApplicationConstants.PROPERTY_FILE_NAME), null);
+            }
+        }
+
+        private boolean hasDatabasePathPropertyChanged(Properties properties) {
+
+            return !(properties.getProperty(URLyBirdApplicationConstants.PROPERTY_FILE_KEY_PATH_TO_DATABASE_FILE)
+                    .equals(serverConfigurationPanel.getPathToDatabaseFileFromTextField())
+
+                    && properties.getProperty(URLyBirdApplicationConstants.PROPERTY_FILE_KEY_RMI_HOSTNAME)
+                    .equals(serverConfigurationPanel.getRmiHostnameFromTextField())
+
+                    && properties.getProperty(URLyBirdApplicationConstants.PROPERTY_FILE_KEY_RMI_PORT_NUMBER)
+                    .equals(serverConfigurationPanel.getRmiPortNumberFromTextField()));
+        }
     }
 }
