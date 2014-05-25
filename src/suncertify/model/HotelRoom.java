597b226bcb683f23051f31bae36d20a9a6e0363f
Luke GJ Potter <lukegjpotter@gmail.com>
2014-05-25 22:56:28 +0100

    Adding JavaDoc to the suncertify.model package.

--- src/suncertify/model/HotelRoom.java
+++ src/suncertify/model/HotelRoom.java
@@ -1,11 +1,12 @@
 package suncertify.model;
 
-
 import suncertify.utilities.UrlyBirdApplicationConstants;
 
 /**
+ * The pojo to represent a Hotel Room.
+ *
  * @author Luke GJ Potter
- * Date: 06/12/2013
+ * @since  06/12/2013
  */
 public class HotelRoom {
 
@@ -18,6 +19,16 @@ public class HotelRoom {
     private String ownerName;
 
     // ---------- Constructors ----------
+    /**
+     * Constructor for the HotelRoom pojo.
+     *
+     * @param name The name of the hotel.
+     * @param location The city that the hotel is located in.
+     * @param roomSize The size of the room.
+     * @param isSmoking Weather the room allows occupants to smoke.
+     * @param rate The cost of the room.
+     * @param date The date that the room will be available from.
+     */
     public HotelRoom(String name, String location, int roomSize, boolean isSmoking, double rate, String date) {
 
         setName(name);
@@ -30,6 +41,8 @@ public class HotelRoom {
     }
 
     /**
+     * Constructor for the HotelRoom pojo.
+     *
      * Because the output of the {@code Data.readRecord(String)} method looks like
      * {@code
      *     0. |Palace                                                          |
@@ -42,7 +55,8 @@ public class HotelRoom {
      * }
      * for an unbooked room, it must be heavily parsed.
      *
-     * @param strings An array of strings ordered to follow the output of he {@code Data.readRecord(String)} method.
+     * @param strings An array of strings ordered to follow the output of he
+     * {@code Data.readRecord(String)} method.
      */
     public HotelRoom(String... strings) {
 
@@ -56,65 +70,135 @@ public class HotelRoom {
     }
 
     // ---------- Getters and Setters ----------
+    /**
+     * Gets the name.
+     *
+     * @return The name of the hotel.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name.
+     *
+     * @param name The name of the hotel.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the location.
+     *
+     * @return The city that the hotel is located in.
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * Sets the location.
+     *
+     * @param location The city that the hotel is located in.
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * Gets the date.
+     *
+     * @return The date that the room will be available from.
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * Sets the date.
+     *
+     * @param date The date that the room will be available from.
+     */
     public void setDate(String date) {
         this.date = date;
     }
 
+    /**
+     * Gets the owner of a room.
+     *
+     * @return The name of the owner of ths HotelRoom.
+     */
     public String getOwnerName() {
         return ownerName;
     }
 
+    /**
+     * Sets the owner of a room.
+     *
+     * @param ownerName The name of the owner of ths HotelRoom.
+     */
     public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
     }
 
+    /**
+     * Gets the room size.
+     *
+     * @return The size of the room.
+     */
     public int getRoomSize() {
         return roomSize;
     }
 
+    /**
+     * Sets the room size.
+     *
+     * @param roomSize The size of the room.
+     */
     public void setRoomSize(int roomSize) {
         this.roomSize = roomSize;
     }
 
+    /**
+     * Gets isSmoking.
+     *
+     * @return Weather the room allows occupants to smoke.
+     */
     public boolean isSmoking() {
         return isSmoking;
     }
 
+    /**
+     * Sets isSmoking.
+     *
+     * @param isSmoking Weather the room allows occupants to smoke.
+     */
     public void setSmoking(boolean isSmoking) {
         this.isSmoking = isSmoking;
     }
 
+    /**
+     * Gets the rate.
+     *
+     * @return The cost of the room.
+     */
     public double getRate() {
         return rate;
     }
 
+    /**
+     * Sets the rate.
+     *
+     * @param rate The cost of the room.
+     */
     public void setRate(double rate) {
         this.rate = rate;
     }
 
     // ---------- Public Methods ----------
-
     /**
+     * Converts the HotelRoom as a String.
      *
      * @return A string representation of the HotelRoom object.
      */
@@ -124,6 +208,7 @@ public class HotelRoom {
     }
 
     /**
+     * Converts the HotelRoom pojo into a String Array.
      *
      * @return The fields of the HotelRoom object as a String Array.
      */
@@ -176,11 +261,18 @@ public class HotelRoom {
     }
 
     // ---------- Private Methods ----------
+    /**
+     * Extracts a double value from a string.
+     *
+     * @param string The string containing the double.
+     * @return The double value contained in the string.
+     */
     private static double extractDoubleFromString(String string) {
 
         StringBuilder stringBuilder = new StringBuilder();
 
         for (int i = 0; i < string.length(); i++) {
+
             if (Character.isDigit(string.charAt(i)) || string.charAt(i) == '.') {
                 stringBuilder.append(string.charAt(i));
             }
