4ed9faccc17d501bd22daadc34821032c1dac294
Luke GJ Potter <lukegjpotter@gmail.com>
2014-05-27 00:06:53 +0100

    Adding JavaDoc for the packages.

---
+++ src/suncertify/controller/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The package for the control logic of the URLyBird Application.
+ *
+ * It contains the classes in the DAO pattern and the MainController class.
+ *
+ * @author Luke GJ Potter
+ * @since  26/05/2014
+ */
+package suncertify.controller;
---
+++ src/suncertify/rmi/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The RMI package contains the networking parts of the URLyBird Application.
+ *
+ * @author Luke GJ Potter
+ * @since 26/05/2014
+ */
+package suncertify.rmi;
