de49ab9d87845e603851b5cb0f1ea1b2524219a3
Luke GJ Potter <lukegjpotter@gmail.com>
2014-05-27 20:35:42 +0100

    Removing the unused application state.

--- src/suncertify/utilities/URLyBirdApplicationMode.java
+++ src/suncertify/utilities/URLyBirdApplicationMode.java
@@ -10,20 +10,12 @@ public enum UrlyBirdApplicationMode {
 
     /**
      * Network mode; only display the
-     * {@code suncertify.gui.client.URLyBirdClientGui} and use network
-     * access.
+     * {@code suncertify.gui.client.URLyBirdClientGui} and use network access.
      *
      * This is the default application mode; to run the application in this mode
      * the user will not specify any Command Line Arguments.
      */
     NETWORKED_CLIENT,
-
-    /**
-     * Server Mode; only display the
-     * {@code suncertify.gui.server.URLyBirdServerGui} for this instance of the
-     * application.
-     */
-    SERVER_ONLY,
 
     /**
      * Standalone mode; only display the
