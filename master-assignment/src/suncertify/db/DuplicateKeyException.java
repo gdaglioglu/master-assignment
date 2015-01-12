1fa7dea9bc3b31ac3935802a5d26e20f3e748549
gdaglioglu <gdaglioglu@gmail.com>
2015-01-12 21:16:05 +0000

    First commit

---
+++ master-assignment/src/suncertify/db/DuplicateKeyException.java
@@ -0,0 +1,13 @@
+package suncertify.db;
+
+public class DuplicateKeyException extends Exception {
+	
+	public DuplicateKeyException(){
+		
+	}
+	
+    public DuplicateKeyException(String message){
+		
+	}
+
+}
---
+++ master-assignment/src/suncertify/db/RecordNotFoundException.java
@@ -0,0 +1,13 @@
+package suncertify.db;
+
+public class RecordNotFoundException extends Exception {
+	
+	public RecordNotFoundException(){
+		
+	}
+	
+    public RecordNotFoundException(String message){
+		
+	}
+
+}
