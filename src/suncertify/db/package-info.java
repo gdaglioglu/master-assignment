2c546712400e27bdec9699058f7e0b7ceecac639
gdaglioglu <gdaglioglu@gmail.com>
2015-01-24 15:33:52 +0000

    deleting unnecessary files

--- src/suncertify/db/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Classes used for database access and record locking.
- * The classes in this package handle reading from and
- * writing to the database.
- */
-package suncertify.db;
--- src/suncertify/presentation/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Classes used to display graphical user interfaces.
- * The classes in this package are primarily used to
- * provide the GUI layer of the application.
- */
-package suncertify.presentation;
